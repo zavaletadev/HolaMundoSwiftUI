3ec97f16409f8ca724a0903a9736b621a3e0c29a
Raúl <raul.zavaletazea@gmail.com>
2022-01-04 02:13:54 -0600

    Initial Commit

---
+++ HolaMundoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HolaMundoSwiftUI
+//
+//  Created by Raul Zavaleta Zea on 04/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HolaMundoSwiftUI/HolaMundoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolaMundoSwiftUIApp.swift
+//  HolaMundoSwiftUI
+//
+//  Created by Raul Zavaleta Zea on 04/01/22.
+//
+
+import SwiftUI
+
+@main
+struct HolaMundoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
